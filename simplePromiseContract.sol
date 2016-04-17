a343dec0ea49a17d16335b005a4a58b76af8cf52
chinara <chinara.isabaeva@gmail.com>
2016-04-17 16:03:46 +0100

    update contract

--- simplePromiseContract.sol
+++ simplePromiseContract.sol
@@ -1,42 +1,44 @@
-contract Promise // is usingOraclize
+contract Promise
 {
-    address public owner;
+    address public politician;
+    address public validator;
+    
     string public url;
     mapping(address => uint) public contributions;
     address[] public contributors;
     
-    function Promise(string _url) {
-        owner = msg.sender;
+    function Promise(string _url, address _validator) {
+        politician = msg.sender;
+        validator = _validator;
         url = _url;
     }
     
-    // Remove. Is part of usingOraclize
-    function strCompare(string a, string b) returns (uint) {
-        return 1;
-    }
-    
-    function __callback(bytes32 id, string result) {
-        if (msg.sender != oraclize_cbAddress()) {
+    function updateStatus(bytes32 id, int8 status) {
+        if (msg.sender != validator) {
             throw;
         }
-        if (strCompare(result, "fulfilled") == 0) {
-            suicide(owner);
-        } else if (strCompare(result, "unsure") == 0) {
-            // Do nothing
-        } else if (strCompare(result, "failed") == 0) {
-            for (uint index = 0; index < contributors.length; index++) {
+        uint index;
+        if (status < 0) {
+            for (index = 0; index < contributors.length; index++) {
                 contributors[index].send(contributions[contributors[index]]);
             }   
+        } else if (status == 0) {
+            for (index = 0; index < contributors.length; index++) {
+                contributors[index].send(contributions[contributors[index]] / 2);
+            }   
+            suicide(politician);
+        } else {
+            suicide(politician);
         }
-    }    
+}    
     
-    function () {
+    function contribute() {
         if (msg.value == 0) {
             throw;
         }
         // There is no limiting the contributing keys, a rich person can create many keys
         if (contributions[msg.sender] == 0) {
-            contributors.psuh(msg.sender);
+            contributors.push(msg.sender);
         }
         contributions[msg.sender] += msg.value;
     }
